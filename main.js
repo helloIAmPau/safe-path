1b4beb3be1268b8a5a024341cae61063c573eba1
Pasquale Boemio <pasquale.boemio@statwolf.com>
2016-11-25 12:09:17 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const pathModule require('path');
+
+const service = {};
+
+service.sanitize = function(path) {
+  return path.replace(/\\/g, '/');
+};
+
+service.join = function() {
+  return Array.from(arguments).map(function(arg) {
+    return service.sanitize(arg);
+  }).join('/');
+};
+
+service.dirname = function(path) {
+  return service.sanitize(pathModule.dirname(path));
+};
+
+service.basename = function(path) {
+  return service.sanitize(pathModule.basename(path));
+};
+
+service.sep = '/';
+
+export default service;
